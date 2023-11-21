1c2f9ec2a5be0a7111eb1f46ec9a583299709824
perty <per@artcomputer.se>
2023-11-21 12:55:36 +0100

    Added some expaining text.

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,35 @@
 port module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, textarea, text)
+import Html exposing (Html, button, div, h1, p, text, textarea)
 import Html.Attributes exposing (class, style, value)
 import Html.Events exposing (onClick, onInput)
 import Markdown
 import Time
+
 
 type alias Model =
     { markdown : String
     , html : Html Msg
     }
 
-init : String -> (Model, Cmd Msg)
+
+init : String -> ( Model, Cmd Msg )
 init savedMarkdown =
     let
-        markdown = if savedMarkdown == "" then "type markdown here" else savedMarkdown
+        markdown =
+            if savedMarkdown == "" then
+                "type markdown here"
+
+            else
+                savedMarkdown
     in
     ( { markdown = markdown
       , html = Markdown.toHtml [] markdown
-      }, Cmd.none
+      }
+    , Cmd.none
     )
+
 
 type Msg
     = MarkdownChanged String
@@ -28,41 +37,56 @@ type Msg
     | SaveMarkdown
     | Tick Time.Posix
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         MarkdownChanged newMarkdown ->
             ( { model | markdown = newMarkdown, html = Markdown.toHtml [] newMarkdown }, Cmd.none )
+
         DownloadMarkdown ->
             ( model, downloadMarkdown model.markdown )
+
         SaveMarkdown ->
             ( model, saveMarkdown model.markdown )
+
         Tick _ ->
             ( model, saveMarkdown model.markdown )
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ textarea 
-            [ style "width" "100%"
-            , style "height" "50vh"
-            , value model.markdown
-            , onInput MarkdownChanged 
-            ] []
-        , button [ onClick DownloadMarkdown ] [ text "Download Markdown" ]
-        , div [class "markdown-output"] [ model.html ]
+        [ h1 [] [ text "Markdown Editor" ]
+        , p [] [ text "This is a simple markdown editor. It will save your markdown every 30 seconds locally in your browser. You can download the markdown by pressing the download button." ]
+        , div []
+            [ textarea
+                [ style "width" "100%"
+                , style "height" "50vh"
+                , value model.markdown
+                , onInput MarkdownChanged
+                ]
+                []
+            , button [ onClick DownloadMarkdown ] [ text "Download Markdown" ]
+            , div [ class "markdown-output" ] [ model.html ]
+            ]
         ]
 
+
 port downloadMarkdown : String -> Cmd msg
+
+
 port saveMarkdown : String -> Cmd msg
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Time.every 30000 Tick
 
+
 main : Program String Model Msg
 main =
-    Browser.element 
+    Browser.element
         { init = init
         , update = update
         , view = view
