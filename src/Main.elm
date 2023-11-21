93cc1229e6a7295959b9c7950655ec501e89cfba
perty <per@artcomputer.se>
2023-11-21 12:47:09 +0100

    Rewrote to be an markdown editor.

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,70 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
-import HelloWorld exposing (helloWorld)
-import Html exposing (Html, div, img)
-import Html.Attributes exposing (src, style)
-import Msg exposing (Msg(..))
-import VitePluginHelper
+import Html exposing (Html, button, div, textarea, text)
+import Html.Attributes exposing (class, style, value)
+import Html.Events exposing (onClick, onInput)
+import Markdown
+import Time
 
+type alias Model =
+    { markdown : String
+    , html : Html Msg
+    }
 
-main : Program () Int Msg
-main =
-    Browser.sandbox { init = 0, update = update, view = view }
+init : String -> (Model, Cmd Msg)
+init savedMarkdown =
+    let
+        markdown = if savedMarkdown == "" then "type markdown here" else savedMarkdown
+    in
+    ( { markdown = markdown
+      , html = Markdown.toHtml [] markdown
+      }, Cmd.none
+    )
 
+type Msg
+    = MarkdownChanged String
+    | DownloadMarkdown
+    | SaveMarkdown
+    | Tick Time.Posix
 
-update : Msg -> number -> number
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        MarkdownChanged newMarkdown ->
+            ( { model | markdown = newMarkdown, html = Markdown.toHtml [] newMarkdown }, Cmd.none )
+        DownloadMarkdown ->
+            ( model, downloadMarkdown model.markdown )
+        SaveMarkdown ->
+            ( model, saveMarkdown model.markdown )
+        Tick _ ->
+            ( model, saveMarkdown model.markdown )
 
-        Decrement ->
-            model - 1
-
-
-view : Int -> Html Msg
+view : Model -> Html Msg
 view model =
     div []
-        [ img [ src <| VitePluginHelper.asset "/src/assets/logotype.png", style "width" "300px" ] []
-        , helloWorld model
+        [ textarea 
+            [ style "width" "100%"
+            , style "height" "50vh"
+            , value model.markdown
+            , onInput MarkdownChanged 
+            ] []
+        , button [ onClick DownloadMarkdown ] [ text "Download Markdown" ]
+        , div [class "markdown-output"] [ model.html ]
         ]
+
+port downloadMarkdown : String -> Cmd msg
+port saveMarkdown : String -> Cmd msg
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Time.every 30000 Tick
+
+main : Program String Model Msg
+main =
+    Browser.element 
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
